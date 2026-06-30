4dab549a7c27880dad01499621000561c87ac7da
Sieghartm4 <sieghartm4@gmail.com>
2026-06-30 11:21:39 +0800

    [UPDATE] fixed .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
   },
+  envDir: path.resolve(__dirname, '..'),
 })
