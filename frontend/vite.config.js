19209f9db1947ffa25cc00399e0838465969572a
Sieghartm4 <sieghartm4@gmail.com>
2026-06-30 11:34:36 +0800

    [UPDATE] FIXED VITE ENV

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-  },
-  envDir: path.resolve(__dirname, '..'),
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, path.resolve(__dirname, ".."), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      host: env.VITE_HOST_FRONTEND || "0.0.0.0",
+      port: parseInt(env.VITE_PORT_FRONTEND) || 5173,
+    },
+    envDir: path.resolve(__dirname, ".."),
+  };
+});
